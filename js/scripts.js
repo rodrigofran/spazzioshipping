b15b1ea79369f1934762b5c137b13837aadb9a10
Rodrigo Francischett <rodrigofrancms@gmail.com>
2022-08-25 20:09:44 -0300

    fix background loader

--- js/scripts.js
+++ js/scripts.js
@@ -62,6 +62,6 @@ var loader = document.querySelector(".loader")
 
 window.addEventListener("load", vanish);
 
-function vanish() {
+setInterval(function vanish() {
     loader.classList.add("disppear");
-}
+},200)
